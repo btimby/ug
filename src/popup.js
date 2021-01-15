f25cd0f1f530cc410c9aca3c53db6c6ce21c24fa
Ben Timby <btimby@gmail.com>
2021-01-15 16:17:38 -0500

    Popup usability.

--- src/popup.js
+++ src/popup.js
@@ -5,6 +5,15 @@ const debug = require('debug')('ug:popup');
 function openView() {
   var hash = $('#url').val();
 
+  if (!hash.startsWith('web+ug://')) {
+    $('#url')
+      .addClass('invalid')
+      .val('');
+    return false;
+  } else {
+    $('#url').removeClass('invalid');
+  }
+
   debug('Opening UG tab: %s', hash);
   browser.tabs.create({
     'url': `/dist/html/view.html?url=${hash}`,
@@ -24,19 +33,27 @@ function openServe() {
   window.close();
 }
 
-function keyUp(ev) {
-  var charCode = (typeof ev.which === 'number') ? ev.which : ev.keyCode;
-
-  if (charCode === 13) {
-    openView();
-  }
-}
-
 if (document) {
   $(document).ready(() => {
     // Set up event handlers.
-    $('#go').on('click', openView);
+    $('#go').on('click', (ev) => {
+      ev.preventDefault();
+      return openView();
+    });
     $('#serve').on('click', openServe);
-    $('#url').on('keyup', keyUp);
+    $('#url')
+      .on('keyup', (ev) => {
+        $('#url').removeClass('invalid');
+        var charCode = (typeof ev.which === 'number') ? ev.which : ev.keyCode;
+
+        if (charCode === 13) {
+          ev.preventDefault();
+          return openView();
+        }
+      })
+      .on('focus', () => {
+        $('#url').removeClass('invalid');
+      });
+    $('#help').on('click', () => $('#readme').show());
   });
 }
